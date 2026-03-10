c92b2ef39d600de5485411c09373d1b8ad805df9
prasannaRobotics-Max <grobotics2.o@gmail.com>
2026-03-10 23:51:34 +0530

    Update WebMVCConfig.java

--- src/main/java/com/example/JobPortal/WebMVCConfig.java
+++ src/main/java/com/example/JobPortal/WebMVCConfig.java
@@ -33,7 +33,7 @@ public class WebMVCConfig implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("GET","POST","PUT","DELETE","OPTIONS").allowedHeaders("*").allowCredentials(true);
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173,https://job-portal-frontend-6dv2.onrender.com").allowedMethods("GET","POST","PUT","DELETE","OPTIONS").allowedHeaders("*").allowCredentials(true);
             }
         };
     }
